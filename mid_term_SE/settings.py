3574cc8044b74aa8a7569ae895e0a921caf77cec
MinhTriet_19120039 <57978985+19120039@users.noreply.github.com>
2022-04-08 12:27:21 +0700

    Video 21+22: Postgres Database + AWS RDS, AWS S3 Buckets
    Trong video này em đã học được:
    - Tạo database bằng phần mềm Postgres Database và pg Admin
    - Kết nối server với Source code và đẩy các file table dữ liệu ta đã nhập từ các video trước lên database này
    - Tạo bucket trên web WAS của Amazon để lưu trữ database

--- mid_term_SE/settings.py
+++ mid_term_SE/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'mid_term_SE.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'DEMO_TEST',
+        'USER': 'postgres',
+        'PASSWORD':'742001',
+        'HOST':'localhost',
+        'PORT': '5432'
     }
 }
 
@@ -120,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
-import os
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
